178cb42e1435d20a4364bfe09e9e256efe6a1226
Joe Longstreet <joelongstreet@gmail.com>
2014-05-06 10:56:02 -0500

    move routes to own lib

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var mongoClient = require('./lib/mongo-client');
 var faces = require('./lib/faces');
+var routes = require('./lib/routes');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,57 +10,10 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/photo-queue'));
 
-app.get('/', function(req, res){
-  res.redirect('/face-verifier.html');
-});
-
-app.get('/all-users', function(req, res){
-  mongoClient.getAllPartners()
-    .then(function(partners){
-
-      partners.sort(function(a, b){
-        if(a.lastName < b.lastName) return -1;
-        if(a.lastName > b.lastName) return 1;
-        return 0;
-      });
-
-      res.send(partners);
-    });
-});
-
-app.get('/next-photo-in-queue', function(req, res){
-  mongoClient.getQueueItem()
-    .then(function(queueItem){
-      res.send(queueItem);
-    });
-});
-
-app.get('/mark-record/:queueId', function(req, res){
-  var userId = req.query.userId;
-  var queueId = req.params.queueId;
-  var fullPhotoPath = path.join(process.cwd(), 'photo-queue', queueId + '.jpg');
-
-  // If no user id is supplied, assume it's a non record
-  // and just delete it from the qeueue
-  if(!userId){
-    mongoClient.deleteQueueItem({
-      queueId: queueId
-    }).then( res.send({ 'updated' : true }) );
-  } else {
-
-    mongoClient.getUserRecord(userId)
-      .then(function(user){
-        user.fullPhoto = fullPhotoPath;
-        createFacesFromPhotos([user]);
-      })
-      .then(function(){
-        mongoClient.deleteQueueItem({
-          queueId: queueId
-        });
-      })
-      .then( res.send({ 'updated' : true }) );
-    }
-  });
+app.get('/', routes.home);
+app.get('/all-users', routes.allUsers);
+app.get('/next-photo-in-queue', routes.nextPhotoInQueue);
+app.get('/mark-record/:queueId', routes.markRecords);
 
 console.log('web app listening on port', port);
 app.listen(port);
