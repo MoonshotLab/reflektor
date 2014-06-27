5d484fea3735afd540d95c080fe08eba78c1be20
Joe Longstreet <joelongstreet@gmail.com>
2014-06-27 08:49:47 -0500

    add keyboard shortcuts to make face verification easier

--- web/public/face-verifier.js
+++ web/public/face-verifier.js
@@ -1,3 +1,19 @@
+Mousetrap.bind('command+j', function(e){
+  e.preventDefault();
+  confirmUser();
+});
+
+Mousetrap.bind('command+d', function(e){
+  e.preventDefault();
+  deleteQueueItem();
+});
+
+Mousetrap.bind('command+k', function(e){
+  e.preventDefault();
+  loadNewPhoto();
+});
+
+
 var loadNewPhoto = function(){
   $.ajax({
     url: '/get-random-photo-in-queue',
@@ -6,6 +22,32 @@ var loadNewPhoto = function(){
       $('#photo').data('queue-id', data._id);
       $('#photo').attr('src', photoPath);
     }
+  });
+};
+
+
+var confirmUser = function(){
+  var queueId = $('#photo').data('queue-id');
+  var userId = $('#user-selector').val();
+  $.ajax({
+    url: '/assign-queue-item/' + queueId + '?userId=' + userId,
+    success: function(res){
+      console.log('Succesfully added photo', res.faceFile, 'to', res.user.firstName, res.user.lastName);
+      loadNewPhoto();
+    }, err: loadNewPhoto
+  });
+};
+
+
+var deleteQueueItem = function(){
+  var queueId = $('#photo').data('queue-id');
+
+  $.ajax({
+    url: '/delete-queue-item/' + queueId,
+    success: function(id){
+      console.log('Successfully deleted photo', id);
+      loadNewPhoto();
+    }, error: loadNewPhoto
   });
 };
 
@@ -26,31 +68,8 @@ $(function(){
     }
   });
 
-
-  $('#confirm-user').click(function(){
-    var queueId = $('#photo').data('queue-id');
-    var userId = $userSelector.val();
-    $.ajax({
-      url: '/assign-queue-item/' + queueId + '?userId=' + userId,
-      success: function(res){
-        console.log('Succesfully added photo', res.faceFile, 'to', res.user.firstName, res.user.lastName);
-        loadNewPhoto();
-      }, err: loadNewPhoto
-    });
-  });
-
-  $('#trash-photo').click(function(){
-    var queueId = $('#photo').data('queue-id');
-
-    $.ajax({
-      url: '/delete-queue-item/' + queueId,
-      success: function(id){
-        console.log('Successfully deleted photo', id);
-        loadNewPhoto();
-      }, error: loadNewPhoto
-    });
-  });
-
+  $('#confirm-user').click(confirmUser);
+  $('#trash-photo').click(deleteQueueItem);
   $('#skip').click(loadNewPhoto);
 
 });
